a6e9242036fa354672a4d46407f067c8fd052910
Alexaslastina <alexaslastina@gmail.com>
2025-10-31 14:15:11 +0200

    first commit

---
+++ js_test/appTest.js
@@ -0,0 +1,27 @@
+$(() => {
+  init();
+})
+
+const init = () => {
+  createPics()
+}
+
+const createPics = () => {
+  let img1 = new ImgClass(".grid_gallery" , "images/pic4.jpeg", "love💞");
+  let img2 = new ImgClass(".grid_gallery" , "images/pic22.jpeg", "love💞");
+  let img3 = new ImgClass(".grid_gallery" , "images/pic30.jpeg", "love💞");
+  let img4 = new ImgClass(".grid_gallery" , "images/pic8.jpeg", "love💞");
+  let img5 = new ImgClass(".grid_gallery" , "images/pic5.jpeg", "love💞");
+  let img6 = new ImgClass(".grid_gallery" , "images/pic16.jpeg", "love💞");
+  let img7 = new ImgClass(".grid_gallery" , "images/pic7.jpeg", "love💞");
+  let img8 = new ImgClass(".grid_gallery" , "images/pic17.jpeg", "love💞");
+  let img9 = new ImgClass(".grid_gallery" , "images/pic9.jpeg", "love💞");
+  let img10 = new ImgClass(".grid_gallery" , "images/pic10.jpeg", "love💞");
+  let img11 = new ImgClass(".grid_gallery" , "images/pic11.jpeg", "love💞");
+  let img12 = new ImgClass(".grid_gallery" , "images/pic35.jpeg", "love💞");
+  let img13 = new ImgClass(".grid_gallery" , "images/pic21.jpeg", "love💞");
+  let img14 = new ImgClass(".grid_gallery" , "images/pic14.jpeg", "love💞");
+  let img15 = new ImgClass(".grid_gallery" , "images/pic15.jpeg", "love💞");
+ 
+}
+
